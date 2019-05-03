524facdd50a08a6fa37c8df9f044b977e3c21a47
gureuso <wyun13043@gmail.com>
2019-05-04 00:08:43 +0900

    Fix theaters, theater_tickets api

--- apps/controllers/v1/theater_tickets/controllers.py
+++ apps/controllers/v1/theater_tickets/controllers.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, request
 
-from apps.common.response import ok
+from apps.common.response import ok, error
 from apps.common.database import db_session
 from apps.models.theater_tickets import TheaterTicket
+from apps.models.showtimes import Showtime
 
 app = Blueprint('v1_theater_tickets', __name__, url_prefix='/v1/theater_tickets')
 
@@ -15,6 +16,10 @@ def create():
     showtime_id = form['showtime_id']
     x = form['x']
     y = form['y']
+
+    showtime = Showtime.query.filter_by(id=showtime_id, theater_id=theater_id).first()
+    if not showtime:
+        return error(40400)
 
     theater_ticket = TheaterTicket(theater_id=theater_id, showtime_id=showtime_id, x=x, y=y)
     db_session.add(theater_ticket)
--- apps/controllers/v1/theaters/controllers.py
+++ apps/controllers/v1/theaters/controllers.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
 
-from apps.common.response import ok
+from apps.common.response import ok, error
+from apps.models.showtimes import Showtime
 from apps.models.theaters import Theater
+from apps.models.theater_tickets import TheaterTicket
 
 app = Blueprint('v1_theaters', __name__, url_prefix='/v1/theaters')
 
 
 @app.route('/<int:theater_id>/showtimes/<int:showtime_id>', methods=['get'])
 def detail(theater_id, showtime_id):
+    showtime = Showtime.query.filter_by(id=showtime_id, theater_id=theater_id).first()
+    if not showtime:
+        return error(40400)
+
     theater = Theater.query.filter_by(id=theater_id).first()
 
     seats = []
@@ -18,9 +24,8 @@ def detail(theater_id, showtime_id):
             'seat_number': '{}-{}'.format(x, y),
             'selected_seat': False
         }
-        for theater_ticket in theater.theater_tickets:
-            if theater_ticket.showtime_id != showtime_id:
-                continue
+        theater_tickets = TheaterTicket.query.filter_by(theater_id=theater_id, showtime_id=showtime_id).all()
+        for theater_ticket in theater_tickets:
             if theater_ticket.x == x and theater_ticket.y == y:
                 data['selected_seat'] = True
         seats.append(data)
